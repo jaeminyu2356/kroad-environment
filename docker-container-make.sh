c7bc66fe0f45062e57ca818b690ca9a199ab0f6f
jm <jaeminyu2356@gmail.com>
2024-07-08 18:42:00 +0900

    container option add

--- docker-container-make.sh
+++ docker-container-make.sh
@@ -1,12 +1,12 @@
 docker build -f docker-base -t kroad:V.1 .
 
-docker run -it 
---privileged -e DISPLAY=$DISPLAY 
---env="QT_X11_NO_MITSHM=1" 
--v="/tmp/.gazebo/:/root/.gazebo/" 
--v /tmp/.X11-unix:/tmp/.X11-unix:ro 
--v /docker_mounted/kroad/:/root/mounted_folder/ 
---hostname kroad 
---network host 
---gpus all 
---name kroad kroad:V.1 bashecho
+docker run -it \
+--privileged -e DISPLAY=$DISPLAY \
+--env="QT_X11_NO_MITSHM=1" \
+-v="/tmp/.gazebo/:/root/.gazebo/" \
+-v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+-v /docker_mounted/kroad/:/root/mounted_folder/ \
+--hostname kroad \
+--network host \
+--gpus all \
+--name kroad kroad:V.1 bash
