ec1661da9a1c8b15849d373d8eb4efe6b7ab9def
jm <jaeminyu2356@gmail.com>
2024-07-09 18:00:07 +0900

    fix xhost error

--- docker-container-make.sh
+++ docker-container-make.sh
@@ -4,11 +4,12 @@ sudo apt upgrade
 docker build -f docker-base -t kroad:V.1 .
 
 docker run -it \
---privileged -e DISPLAY=$DISPLAY \
+-e DISPLAY=$DISPLAY \
+--privileged \
 --env="QT_X11_NO_MITSHM=1" \
 -v="/tmp/.gazebo/:/root/.gazebo/" \
 -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
 -v /docker_mounted/kroad/:/root/mounted_folder/ \
 --hostname kroad \
 --network host \
---name kroad kroad:V.1 bash
+--name kroad kroad:V.2 bash
