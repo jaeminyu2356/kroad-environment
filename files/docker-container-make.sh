ab9db35cc8d1220821c76afb538a61d3631fc916
Yujaemin <71584316+jaeminyu2356@users.noreply.github.com>
2024-07-10 17:13:19 +0900

    Update docker-container-make.sh

--- files/docker-container-make.sh
+++ files/docker-container-make.sh
@@ -1,7 +1,7 @@
 sudo apt update
 sudo apt upgrade
 
-docker build -f docker-base -t kroad:V.2 .
+docker build -f docker-base -t kroad:V.2.1 .
 
 docker run -it \
 -e DISPLAY=$DISPLAY \
@@ -9,7 +9,7 @@ docker run -it \
 --env="QT_X11_NO_MITSHM=1" \
 -v="/tmp/.gazebo/:/root/.gazebo/" \
 -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
--v /docker_mounted/kroad/:/root/mounted_folder/ \
+-v /docker_mounted/kroad_v2/:/root/mounted_folder/ \
 --hostname kroad \
 --network host \
---name kroad kroad:V.2 bash
+--name kroad_v2 kroad:V.2.1 bash
