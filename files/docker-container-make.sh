e42b78977d8e2c1c18df7bfadffc4535270349b3
jm <jaeminyu2356@gmail.com>
2024-07-10 18:27:17 +0900

    fix mount error

--- files/docker-container-make.sh
+++ files/docker-container-make.sh
@@ -8,9 +8,9 @@ docker run -it \
 -e DISPLAY=$DISPLAY \
 --privileged \
 --env="QT_X11_NO_MITSHM=1" \
--v="/tmp/.gazebo/:/root/.gazebo/" \
+-v "/tmp/.gazebo/:/root/.gazebo/" \
 -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
--v /docker_mounted/kroad_v2/:/root/mounted_folder/ \
+-v ~/docker_mounted/kroad/:/root/mounted_folder/ \
 --hostname kroad \
 --network host \
 --name kroad_v2 kroad:V.2.1 bash
